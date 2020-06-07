3d90249f91cadfd8fa0f65fe946410cf4e9c7860
Limon Monte <limon.monte@gmail.com>
2020-06-07 20:06:05 +0300

    bump

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const checkRTL = (e: KeyboardEvent) => {
   const container = editor.getSelection().commonAncestorContainer as HTMLElement
   if (container && container.textContent?.length === 0) {
     // ranges are taken from https://stackoverflow.com/a/19143254
-    const ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF' + '\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF'
+    const ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0800-\u1FFF' + '\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF'
     const rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC'
     const ltrDirCheck = new RegExp('[' + ltrChars + ']')
     const rtlDirCheck = new RegExp('[' + rtlChars + ']')
